5589ce1452d0452c0c8b84c364aef710012de528
Anatol Belski <ab@php.net>
2016-09-28 02:05:33 +0200

    fix setvars batch

--- bin/phpsdk_setvars.bat
+++ bin/phpsdk_setvars.bat
@@ -1,9 +1,9 @@
-@echo ofF
+rem @echo ofF
 
 rem Add necessary dirs to the path 
-set PHP_SDK_BIN_PATH=%~dp0\..
-set PHP_SDK_PATH=%PHP_SDK_BIN_PATH%\..
-set PHP_SDK_MSYS2_PATH=%PHP_SDK_PATH%\..\msys2\usr\bin
+set PHP_SDK_BIN_PATH=%~dp0
+set PHP_SDK_PATH=%PHP_SDK_BIN_PATH%..
+set PHP_SDK_MSYS2_PATH=%PHP_SDK_PATH%\msys2\usr\bin
 
 set PATH=%PHP_SDK_BIN_PATH%;%PHP_SDK_MSYS2_PATH%;%PATH%
 
