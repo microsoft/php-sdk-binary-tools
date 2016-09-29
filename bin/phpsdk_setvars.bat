312d4bbc26e2495217c6fbee5e81323c5b9a16a3
Anatol Belski <ab@php.net>
2016-09-29 18:19:01 +0200

    add php executable for binary tools internal usage

--- bin/phpsdk_setvars.bat
+++ bin/phpsdk_setvars.bat
@@ -4,6 +4,7 @@ rem Add necessary dirs to the path
 set PHP_SDK_BIN_PATH=%~dp0
 set PHP_SDK_PATH=%PHP_SDK_BIN_PATH%..
 set PHP_SDK_MSYS2_PATH=%PHP_SDK_PATH%\msys2\usr\bin
+set PHP_SDK_PHP_CMD=%PHP_SDK_BIN_PATH%php\do_php.bat
 
 set PATH=%PHP_SDK_BIN_PATH%;%PHP_SDK_MSYS2_PATH%;%PATH%
 
