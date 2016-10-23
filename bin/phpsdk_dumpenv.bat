f1c27b9ff5995be43fdf01ad8fad65fb60a5eacf
Anatol Belski <ab@php.net>
2016-10-23 19:22:05 +0200

    add dumpenv and rename a bit

---
+++ bin/phpsdk_dumpenv.bat
@@ -0,0 +1,7 @@
+@echo off
+
+call %PHP_SDK_BIN_PATH%\phpsdk_version.bat
+
+echo PHP SDK path:  %PHP_SDK_PATH%
+echo ARCH: %PHP_SDK_ARCH%
+echo VC++: %PHP_SDK_VC%
--- phpsdk-vc14-x86.bat
+++ phpsdk-vc14-x86.bat
@@ -4,9 +4,9 @@ setlocal enableextensions enabledelayedexpansion
 
 set PHP_SDK_RUN_FROM_ROOT=1
 
-call %~dp0bin\phpsdk_shell.bat vc14 x86
+call %~dp0bin\phpsdk_setshell.bat vc14 x86
 
-cmd /k %PHP_SDK_SHELL_CMD% && %~dp0\bin\phpsdk_setvars.bat
+cmd /k "%PHP_SDK_SHELL_CMD% && %~dp0\bin\phpsdk_setvars.bat && %~dp0\bin\phpsdk_dumpenv.bat"
 
 set PHP_SDK_RUN_FROM_ROOT=
 
