b00a9bfeb4bd5f235b68766251df6bfbd4024887
Anatol Belski <ab@php.net>
2016-10-23 20:03:20 +0200

    strip trailing slashes from the basic path vars

--- bin/phpsdk_setvars.bat
+++ bin/phpsdk_setvars.bat
@@ -1,10 +1,17 @@
 @echo off
 
 rem Add necessary dirs to the path 
+
 set PHP_SDK_BIN_PATH=%~dp0
-set PHP_SDK_PATH=%PHP_SDK_BIN_PATH%..
+rem remove trailing slash
+set PHP_SDK_BIN_PATH=%PHP_SDK_BIN_PATH:~0,-1%
+
+for %%a in ("%PHP_SDK_BIN_PATH%") do set PHP_SDK_PATH=%%~dpa
+rem remove trailing slash
+set PHP_SDK_PATH=%PHP_SDK_PATH:~0,-1%
+
 set PHP_SDK_MSYS2_PATH=%PHP_SDK_PATH%\msys2\usr\bin
-set PHP_SDK_PHP_CMD=%PHP_SDK_BIN_PATH%php\do_php.bat
+set PHP_SDK_PHP_CMD=%PHP_SDK_BIN_PATH%\php\do_php.bat
 
 set PATH=%PHP_SDK_BIN_PATH%;%PHP_SDK_MSYS2_PATH%;%PATH%
 
