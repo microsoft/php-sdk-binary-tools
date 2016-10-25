baf09da3959c09070c44a87478987f2ee5364346
Anatol Belski <ab@php.net>
2016-10-26 01:17:33 +0200

    remove unsupported branches

--- bin/phpsdk_buildtree.bat
+++ bin/phpsdk_buildtree.bat
@@ -4,26 +4,7 @@ IF "%1" EQU "" GOTO Help
 IF "%2" NEQ ""   SET _=%2\%1
 IF "%2" EQU ""   SET _=%CD%\%1
 
-MD %_%\vc9\x86\deps\bin
-MD %_%\vc9\x86\deps\lib
-MD %_%\vc9\x86\deps\include
-MD %_%\vc9\x64\deps\bin
-MD %_%\vc9\x64\deps\lib
-MD %_%\vc9\x64\deps\include
-
-MD %_%\vc11\x86\deps\bin
-MD %_%\vc11\x86\deps\lib
-MD %_%\vc11\x86\deps\include
-MD %_%\vc11\x64\deps\bin
-MD %_%\vc11\x64\deps\lib
-MD %_%\vc11\x64\deps\include
-
-MD %_%\vc12\x86\deps\bin
-MD %_%\vc12\x86\deps\lib
-MD %_%\vc12\x86\deps\include
-MD %_%\vc12\x64\deps\bin
-MD %_%\vc12\x64\deps\lib
-MD %_%\vc12\x64\deps\include
+rem TODO rewrite this script to handle more intelligently, creating dirs for only the branch matching current env
 
 MD %_%\vc14\x86\deps\bin
 MD %_%\vc14\x86\deps\lib
