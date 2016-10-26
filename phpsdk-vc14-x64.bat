23a3eb24730ae682f3b2923d4374874103137437
Anatol Belski <ab@php.net>
2016-10-26 02:07:10 +0200

    set console title when run from the starter script

--- phpsdk-vc14-x64.bat
+++ phpsdk-vc14-x64.bat
@@ -6,6 +6,7 @@ set PHP_SDK_RUN_FROM_ROOT=1
 
 call %~dp0bin\phpsdk_setshell.bat vc14 x64
 
+title PHP SDK
 cmd /k "%PHP_SDK_VC_SHELL_CMD% && %~dp0\bin\phpsdk_setvars.bat && %~dp0\bin\phpsdk_dumpenv.bat && set prompt=$P$_$+$$$S"
 
 set PHP_SDK_RUN_FROM_ROOT=
