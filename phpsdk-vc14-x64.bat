296be1b3b45c3031f5bf0eb865765da715874437
Anatol Belski <ab@php.net>
2016-10-26 02:00:53 +0200

    make the prompt a bit more user friendly

--- phpsdk-vc14-x64.bat
+++ phpsdk-vc14-x64.bat
@@ -6,7 +6,7 @@ set PHP_SDK_RUN_FROM_ROOT=1
 
 call %~dp0bin\phpsdk_setshell.bat vc14 x64
 
-cmd /k "%PHP_SDK_VC_SHELL_CMD% && %~dp0\bin\phpsdk_setvars.bat && %~dp0\bin\phpsdk_dumpenv.bat"
+cmd /k "%PHP_SDK_VC_SHELL_CMD% && %~dp0\bin\phpsdk_setvars.bat && %~dp0\bin\phpsdk_dumpenv.bat && set prompt=$P$_$+$$$S"
 
 set PHP_SDK_RUN_FROM_ROOT=
 
