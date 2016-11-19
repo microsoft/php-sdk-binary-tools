9dc14d6f1eddfed1cbd38ebbee9fc1244ca6db79
Anatol Belski <ab@php.net>
2016-11-19 16:49:39 +0100

    accept also task arguments

--- phpsdk-starter.bat
+++ phpsdk-starter.bat
@@ -6,9 +6,10 @@ set IMHERE=%~dp0
 
 :getopt
 if /i "%1" equ "-h" goto help
-if /i "%1" equ "-c" set CRT=%2& shift
-if /i "%1" equ "-a" set ARCH=%2& shift
-if /i "%1" equ "-t" set TASK=%2& shift
+if /i "%1" equ "-c" set CRT=%2 & shift
+if /i "%1" equ "-a" set ARCH=%2 & shift
+if /i "%1" equ "-t" set TASK=%2 & shift
+if /i "%1" equ "--task-args" set TASK_ARGS=%2 & shift
 shift
 if not (%1)==() goto getopt
 
@@ -22,6 +23,9 @@ goto skip_help
 
 :skip_help
 
+set CRT=%CRT: =%
+set ARCH=%ARCH: =%
+
 set PHP_SDK_RUN_FROM_ROOT=1
 
 title PHP SDK
@@ -34,10 +38,12 @@ if errorlevel 3 (
 
 if "%TASK%" neq "" (
 	if exist "%TASK%" (
+		set TASK_ARGS=%TASK_ARGS:"=%
+
 		if exist "%IMHERE%phpsdk-local.bat" (
-			cmd /c "!PHP_SDK_VC_SHELL_CMD! && %IMHERE%\bin\phpsdk_setvars.bat && %IMHERE%\bin\phpsdk_dumpenv.bat && %IMHERE%\phpsdk-local.bat && %TASK%"
+			cmd /c "!PHP_SDK_VC_SHELL_CMD! && %IMHERE%\bin\phpsdk_setvars.bat && %IMHERE%\bin\phpsdk_dumpenv.bat && %IMHERE%\phpsdk-local.bat && %TASK% !TASK_ARGS!"
 		) else (
-			cmd /c "!PHP_SDK_VC_SHELL_CMD! && %IMHERE%\bin\phpsdk_setvars.bat && %IMHERE%\bin\phpsdk_dumpenv.bat && %TASK%"
+			cmd /c "!PHP_SDK_VC_SHELL_CMD! && %IMHERE%\bin\phpsdk_setvars.bat && %IMHERE%\bin\phpsdk_dumpenv.bat && %TASK% !TASK_ARGS!"
 		)
 		exit /b
 	) else (
