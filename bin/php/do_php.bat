bb745349a906c01a9c0916964def6f12295f754f
Anatol Belski <ab@php.net>
2017-05-12 22:18:00 +0200

    reuse msys2 certs for curl

--- bin/php/do_php.bat
+++ bin/php/do_php.bat
@@ -1,3 +1,3 @@
 @echo ofF
 
-%~dp0php.exe -c %~dp0php.ini -d extension_dir=%~dp0ext %*
+%~dp0php.exe -c %~dp0php.ini -d curl.cainfo=%PHP_SDK_ROOT_PATH%\msys2\usr\ssl\cert.pem -d extension_dir=%~dp0ext %*
