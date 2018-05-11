9ad145c1549b5181cf34f063c94eca6bdf5db25b
Anatol Belski <ab@php.net>
2018-05-11 14:11:37 +0200

    Increase training runs number for symfony_demo_pdo_mysql

--- pgo/cases/symfony_demo_pdo_mysql/TrainingCaseHandler.php
+++ pgo/cases/symfony_demo_pdo_mysql/TrainingCaseHandler.php
@@ -16,7 +16,7 @@ class TrainingCaseHandler extends Abstracts\TrainingCase implements Interfaces\T
 	protected $nginx;
 	protected $maria;
 	protected $php;
-	protected $max_runs = 1;
+	protected $max_runs = 4;
 
 	public function __construct(Config $conf, ?Interfaces\Server $nginx, ?Interfaces\Server\DB $srv_db)
 	{
